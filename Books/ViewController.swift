5eb9fcb392f1f42f5e681ead5c0673e3fb859c45
SDS-018 <sds-018@70-12-246-156.pools.spcsdns.net>
2017-04-27 13:20:52 +0900

    Books 초기 작업 Action Controller 추가

--- Books/ViewController.swift
+++ Books/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        openAlert()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +22,36 @@ class ViewController: UIViewController {
     }
 
 
+    func openAlert(){
+        let alert = UIAlertController(title: "환영합니다", message: "Books 앱을 시작합니다", preferredStyle: UIAlertControllerStyle.alert)    //actionSheet)    //alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: UIAlertActionStyle.default, handler: nil)
+      /*
+        let cancelAction = UIAlertAction(title: "취소", style: UIAlertActionStyle.cancel, handler: { (action:UIAlertAction) -> () in
+                print(action.title)
+                self.view.backgroundColor = UIColor.brown
+        })
+        */
+        
+        
+        let cancelAction = UIAlertAction(title: "취소", style: UIAlertActionStyle.cancel){
+            (action:UIAlertAction) -> () in
+            self.view.backgroundColor = UIColor.brown
+        }
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        //self.present(alert, animated: true, completion: nil)
+        //self.present(alert, animated: true, completion: (() -> Void))
+        self.present(alert, animated: true){
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: false, block: {
+                (Timer) -> Void in
+                alert.dismiss(animated: true, completion: nil)
+                })
+        }
+        
+        
+    }
 }
 
